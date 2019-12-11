745b67be188c9574861c5a574d1092bec22b1aba
kuncevic <kncvch@gmail.com>
2019-12-11 12:37:22 +1100

    fix console log message

--- src/app/shared/services/lazy.service.ts
+++ src/app/shared/services/lazy.service.ts
@@ -6,6 +6,6 @@ import { Injectable } from '@angular/core';
 export class LazyService {
 
   constructor() { 
-    console.log('I am lazy service "providedIn: \'root\'" imported in the app.module')
+    console.log('I am lazy service "providedIn: \'root\'" used in lazy loaded module')
   }
 }
